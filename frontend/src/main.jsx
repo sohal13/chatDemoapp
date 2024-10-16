9b37c7ccf0ac85a4671efc6928b19a799352fe00
sohal13 <sohalr1305@gmail.com>
2024-10-16 21:58:52 +0530

    #

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthContextProvider } from './context/AuthContext.jsx'
-import { SocketContextProvider } from './context/socketContext.jsx'
+import { AuthContextProvider } from './context/AuthContext'
+import { SocketContextProvider } from './context/socketContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
